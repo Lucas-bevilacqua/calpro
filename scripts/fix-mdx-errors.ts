1eaf8abd6f7d157ef22a605480c510330e3549ee
gitmkt123 <lucas.bevilacqua@idealtrends.com.br>
2025-11-24 15:48:31 -0300

    fix: add third post with MDX error

--- scripts/fix-mdx-errors.ts
+++ scripts/fix-mdx-errors.ts
@@ -10,7 +10,8 @@ const PROBLEMATIC_SLUGS = [
     'financiamento-imobiliario-como-calcular-parcelas-e-escolher-a-melhor-opcao',
     'iss-para-prestadores-de-servico-como-calcular-3128',
     'como-a-inflacao-afeta-seu-dinheiro-calculadora-pratica-1856',
-    'emprestimo-pessoal-como-calcular-juros-e-parcelas-7124'
+    'emprestimo-pessoal-como-calcular-juros-e-parcelas-7124',
+    '13-salario-2024-como-calcular-e-quando-receber-8519'
 ]
 
 async function unpublishProblematicPosts() {
